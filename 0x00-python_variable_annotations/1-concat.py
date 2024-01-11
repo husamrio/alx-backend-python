214db68e7271fb8cab18753cd3a98899f1de8387
husamrio <zulfiqrd@gmail.com>
2024-01-11 11:33:15 +0300

    Basic annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Funct concat takes str1 & str2 as arg returns
+   concatenated string
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Task 1's Module.
+    '''
+    return str1 + str2
